63554413d9f98f6f170ecc65134915b0c17017ab
Florian Decker <florian.decker@lhind.dlh.de>
2019-12-10 05:50:38 +0100

    Added solutions for day 9 of the advent coding calendar 2019 and refactored code.

--- adventofcode/src/main/kotlin/de/florian/adventofcode/y2019/Day07.kt
+++ adventofcode/src/main/kotlin/de/florian/adventofcode/y2019/Day07.kt
@@ -1,8 +1,10 @@
 package de.florian.adventofcode.y2019
 
 import de.florian.adventofcode.AoCDay
+import de.florian.adventofcode.util.CollectionsUtil
 import de.florian.adventofcode.util.permute
-import java.util.*
+import java.math.BigInteger
+import java.util.LinkedList
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
 
@@ -12,13 +14,11 @@ fun main() {
 }
 
 class Day07 : AoCDay() {
-    private val memory = convertToOpcodes(Inputs_2019.DAY_07)
-    private fun convertToOpcodes(input: String): (IntArray) = input.split(",").map { it.toInt() }.toIntArray()
-
+    private val memory = CollectionsUtil.getMemory(Inputs_2019.DAY_07)
 
     override fun part1(): String {
-        val permThrusterOutput = mutableMapOf<List<Int>, Int>()
-        val perms = listOf(0, 1, 2, 3, 4).permute()
+        val permThrusterOutput = mutableMapOf<List<BigInteger>, BigInteger>()
+        val perms = listOf(0, 1, 2, 3, 4).map { it.toBigInteger() }.permute()
 
         for (perm in perms) {
             var programs = mutableListOf<ComputerProgram>()
@@ -27,7 +27,7 @@ class Day07 : AoCDay() {
                 comp.inputs.put(value)
                 programs.add(comp)
             }
-            programs[0].inputs.put(0)
+            programs[0].inputs.put(BigInteger.ZERO)
             for (i in 0 until programs.size) {
                 val compRes = programs[i].run().last()
                 if (i < programs.size - 1) {
@@ -42,17 +42,17 @@ class Day07 : AoCDay() {
     }
 
     override fun part2(): String {
-        val permThrusterOutput = mutableMapOf<List<Int>, Int>()
-        val perms = listOf(5, 6, 7, 8, 9).permute()
+        val permThrusterOutput = mutableMapOf<List<BigInteger>, BigInteger>()
+        val perms = listOf(5, 6, 7, 8, 9).map { it.toBigInteger() }.permute()
         for (perm in perms) {
-            val programs = Array(perm.size) { ComputerProgram(memory, "Computer-${(65 + it).toChar()}") }
+            val programs = Array(perm.size) { ComputerProgram(memory, name = "Computer-${(65 + it).toChar()}") }
             for (i in programs.indices) {
                 programs[Math.floorMod(i - 1, programs.size)].outputs.put(perm[i])
                 programs[i].inputs = programs[Math.floorMod(i - 1, programs.size)].outputs
             }
-            programs[programs.size - 1].outputs.put(0)
+            programs[programs.size - 1].outputs.put(BigInteger.ZERO)
             val executor = Executors.newFixedThreadPool(programs.size)
-            val futures = LinkedList<Future<Pair<String, Int>>>()
+            val futures = LinkedList<Future<Pair<String, BigInteger>>>()
             for (i in programs.indices) {
                 futures.add(executor.submit(programs[i]))
             }
