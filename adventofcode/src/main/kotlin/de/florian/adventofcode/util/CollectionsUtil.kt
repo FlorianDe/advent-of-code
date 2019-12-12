7952a8aecf959366a4661317d51b90ef58e02025
FlorianDe <florian.decker@uni-oldenburg.de>
2019-12-12 01:03:51 +0100

    Refactored code and added some common classes/util functions.

--- adventofcode/src/main/kotlin/de/florian/adventofcode/util/CollectionsUtil.kt
+++ adventofcode/src/main/kotlin/de/florian/adventofcode/util/CollectionsUtil.kt
@@ -2,6 +2,17 @@ package de.florian.adventofcode.util
 
 import java.math.BigInteger
 
+fun <T,V> Array<Array<T>>.indices2DFiltered(predicate: (T) -> Boolean, action: (y: Int, x: Int) -> V): MutableList<V> {
+    val elements = mutableListOf<V>()
+    for(y in this.indices){
+        for(x in this[y].indices){
+            if(predicate(this[y][x])) {
+                elements.add(action(y, x))
+            }
+        }
+    }
+    return elements
+}
 abstract class CollectionsUtil {
     class Store<K, V : Enum<V>>(values: Array<V>, supplier: (V) -> (Pair<K, V>)) {
         private val map: Map<K, V> = values.map(supplier).toMap()
--- adventofcode/src/main/kotlin/de/florian/adventofcode/util/Point.kt
+++ adventofcode/src/main/kotlin/de/florian/adventofcode/util/Point.kt
@@ -1,4 +1,18 @@
 package de.florian.adventofcode.util
 
-class Point {
+import kotlin.math.abs
+import kotlin.math.atan2
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+data class Point(val x: Int, val y: Int){
+    fun manhattanDistance(point: Point): Int {
+        return abs(this.x - point.x) + abs(this.y - point.y)
+    }
+    fun dist(p: Point) : Double = dist(this, p)
+    fun angle(p: Point) : Double = angle(this, p)
+    companion object {
+        fun dist(a: Point, b: Point) : Double  = sqrt((b.y - a.y).toDouble().pow(2) + (b.y - a.y).toDouble().pow(2))
+        fun angle(from: Point, to: Point) = atan2((to.x - from.x).toDouble(), (to.y - from.y).toDouble())
+    }
 }
