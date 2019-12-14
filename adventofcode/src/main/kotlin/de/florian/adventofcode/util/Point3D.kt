e030e074d899df7b6775a984ee42b81035382c1b
FlorianDe <florian.decker@uni-oldenburg.de>
2019-12-14 12:20:01 +0100

    Added solutions for day 12 of the advent coding calendar 2019. Needs some refactoring!

--- adventofcode/src/main/kotlin/de/florian/adventofcode/util/Point3D.kt
+++ adventofcode/src/main/kotlin/de/florian/adventofcode/util/Point3D.kt
@@ -2,35 +2,18 @@ package de.florian.adventofcode.util
 
 import kotlin.math.*
 
-data class Point(var x: Int, var y: Int){
-    enum class Angle(val rad: Double){
-        DEG_90(PI/2),
-        DEG_180(PI/2),
-        DEG_270(2*PI*(3/4.0))
-    }
+data class Point3D(var x: Int, var y: Int, var z: Int){
+    fun dist(p: Point3D) : Double = dist(this, p)
 
-    fun manhattanDistance(point: Point): Int {
-        return abs(this.x - point.x) + abs(this.y - point.y)
-    }
-    fun dist(p: Point) : Double = dist(this, p)
-    fun angle(p: Point) : Double = angle(this, p)
+    fun absSum() : Int = abs(x) +abs(y)+abs(z)
 
-    fun rotate(angle: Angle, c: Point = Point(0, 0)) : Point{
-        return Point(
-            (cos(angle.rad) * (this.x - c.x) + sin(angle.rad) * (this.y - c.y) + c.x).toInt(),
-            (-sin(angle.rad) * (this.x - c.x) + cos(angle.rad) * (this.y - c.y) + c.y).toInt()
-        )
-    }
-
-    operator fun plusAssign(p: Point) {
+    operator fun plusAssign(p: Point3D) {
         this.x += p.x
         this.y += p.y
+        this.z += p.z
     }
 
     companion object {
-        fun dist(a: Point, b: Point) : Double  = sqrt((b.y - a.y).toDouble().pow(2) + (b.y - a.y).toDouble().pow(2))
-        fun angle(from: Point, to: Point) = atan2((to.x - from.x).toDouble(), (to.y - from.y).toDouble())
-        fun minValues(a: Point, b: Point) = Point(min(a.x, b.x), min(a.y, b.y))
-        fun maxValues(a: Point, b: Point) = Point(max(a.x, b.x), max(a.y, b.y))
+        fun dist(a: Point3D, b: Point3D) : Double  = sqrt((b.y - a.y).toDouble().pow(2) + (b.y - a.y).toDouble().pow(2) + (b.z - a.z).toDouble().pow(2))
     }
 }
