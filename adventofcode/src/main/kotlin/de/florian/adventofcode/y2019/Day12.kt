8babae4f55aa01990fed7a8b01966eea082f2258
FlorianDe <florian.decker@uni-oldenburg.de>
2019-12-14 12:20:01 +0100

    Added solutions for day 12 of the advent coding calendar 2019. Needs some refactoring!

--- adventofcode/src/main/kotlin/de/florian/adventofcode/y2019/Day12.kt
+++ adventofcode/src/main/kotlin/de/florian/adventofcode/y2019/Day12.kt
@@ -1,27 +1,135 @@
 package de.florian.adventofcode.y2019
 
 import de.florian.adventofcode.AoCDay
-import kotlin.math.floor
-import kotlin.streams.asSequence
+import de.florian.adventofcode.util.Point3D
+import de.florian.adventofcode.util.permute
+import java.time.LocalDateTime
+import javax.swing.plaf.metal.MetalLookAndFeel
 
 fun main() {
-    Day01().exec()
+    Day12().exec()
 }
 
-class Day01 : AoCDay() {
-    fun String.splittedDoubleSequence() = this.split("\n").stream().asSequence().map { it.toDouble() }
-    val formula: (Double) -> Double = { floor(it / 3.0) - 2 }
-    fun recFormula(it: Double): (Double) = if (formula(it) < 0) 0.0 else formula(it) + recFormula(formula(it))
+class Day12 : AoCDay() {
+    private val inputRegex = """<x=([-]?\d+), y=([-]?\d+), z=([-]?\d+)>""".toRegex()
+
+    fun convertInput(input: String) = input.split("\n").map {
+        val groups = inputRegex.find(it)?.groups
+        lateinit var planet: Planet
+
+        groups?.let { g ->
+            g[1]?.let { x ->
+                g[2]?.let { y ->
+                    g[3]?.let { z ->
+                        planet = Planet(Point3D(Integer.valueOf(x.value), Integer.valueOf(y.value), Integer.valueOf(z.value)))
+                    }
+                }
+            }
+        }
+        planet
+    }
+
+    private fun simulationStep(planets: List<Planet>) : List<Planet>{
+        var from = 1
+        for (a in planets.indices) {
+            for (b in from until planets.size) {
+                if (planets[a].pos.x > planets[b].pos.x) {
+                    planets[a].velocity.x -= 1
+                    planets[b].velocity.x += 1
+                } else if (planets[a].pos.x < planets[b].pos.x) {
+                    planets[a].velocity.x += 1
+                    planets[b].velocity.x -= 1
+                }
+
+                if (planets[a].pos.y > planets[b].pos.y) {
+                    planets[a].velocity.y -= 1
+                    planets[b].velocity.y += 1
+                } else if (planets[a].pos.y < planets[b].pos.y) {
+                    planets[a].velocity.y += 1
+                    planets[b].velocity.y -= 1
+                }
+
+                if (planets[a].pos.z > planets[b].pos.z) {
+                    planets[a].velocity.z -= 1
+                    planets[b].velocity.z += 1
+                } else if (planets[a].pos.z < planets[b].pos.z) {
+                    planets[a].velocity.z += 1
+                    planets[b].velocity.z -= 1
+                }
+            }
+            from++
+        }
+
+        for (planet in planets) {
+            planet.pos += planet.velocity
+        }
+
+        return planets.map{it.copy()}
+    }
+
 
     override fun part1(): String {
-        return Inputs_2019.DAY_01.splittedDoubleSequence()
-            .map(formula)
-            .sum().toInt().toString()
+        val planets = convertInput(Inputs_2019.DAY_12)
+
+        for(step in 1..1000) {
+            simulationStep(planets)
+        }
+
+        return planets.map { it.pos.absSum() * it.velocity.absSum() }.sum().toString()
     }
 
     override fun part2(): String {
-        return Inputs_2019.DAY_01.splittedDoubleSequence()
-            .map { recFormula(it) }
-            .sum().toInt().toString()
+        val planets = convertInput(Inputs_2019.DAY_12)
+        val statesX = HashSet<String>(100_000)
+        val statesY = HashSet<String>(100_000)
+        val statesZ = HashSet<String>(100_000)
+        statesX.add(planets.idx())
+        statesY.add(planets.idy())
+        statesZ.add(planets.idz())
+
+        var step: Long = 1
+        var stepsX: Long  = 0
+        var stepsY: Long  = 0
+        var stepsZ: Long  = 0
+        while(true) {
+            val state = simulationStep(planets)
+            if(stepsX == 0L && statesX.contains(state.idx())){
+                stepsX = step
+                println("X: $stepsX")
+            }
+            if(stepsY == 0L && statesY.contains(state.idy())){
+                stepsY = step
+                println("Y: $stepsX")
+            }
+            if(stepsZ == 0L && statesZ.contains(state.idz())){
+                stepsZ = step
+                println("Z: $stepsX")
+            }
+            if(stepsX > 0 && stepsY > 0 && stepsZ > 0){
+                break
+            }
+            statesX.add(state.idx())
+            statesY.add(state.idy())
+            statesZ.add(state.idz())
+            step++
+        }
+        // LCM = (n1 * n2) / GCD
+        MetalLookAndFeel.getControlDisabled()
+        return lcm(lcm(stepsX, stepsY), stepsZ).toString()
     }
+
+    fun gcd(a: Long, b: Long): Long {
+        if (b == 0L) return a
+        return gcd(b, a % b)
+    }
+
+    fun lcm(a: Long, b: Long): Long {
+        return a / gcd(a, b) * b
+    }
+
+    fun List<Planet>.idx() = this.joinToString(separator = "") { "${it.pos.x}${it.velocity.x}" }
+    fun List<Planet>.idy() = this.joinToString(separator = "") { "${it.pos.y}${it.velocity.y}" }
+    fun List<Planet>.idz() = this.joinToString(separator = "") { "${it.pos.z}${it.velocity.z}" }
+    data class Planet(var pos: Point3D, var velocity: Point3D = Point3D(0,0,0))
 }
+
