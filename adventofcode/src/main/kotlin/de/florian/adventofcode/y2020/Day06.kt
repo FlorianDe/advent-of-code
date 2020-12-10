35cb446346d6f8ab7217d9b6245b838bb3e3e8ad
FlorianDe <florian.decker@alchemisten.ag>
2020-12-10 23:26:00 +0100

    Added solutions for day 06 of the advent coding calendar 2020.

---
+++ adventofcode/src/main/kotlin/de/florian/adventofcode/y2020/Day06.kt
@@ -0,0 +1,29 @@
+import de.florian.adventofcode.AoCDay
+
+fun main() { Day06().exec() }
+
+class Day06 : AoCDay() {
+    private fun convertInput(input: String): List<List<MutableSet<Char>>> = input.split(Regex("\\n\\n")).map { group ->
+        group.trim().split("\n").map { it.toSortedSet()}
+    }
+
+    private val answers = convertInput(Inputs_2020.DAY_06)
+
+    override fun part1(): String {
+        return answers.map {
+            it.fold<Set<Char>, Set<Char>>(mutableSetOf(), { acc, e -> (acc + e) }).size
+        }.sum().toString()
+    }
+
+    override fun part2(): String {
+        return answers.map { group ->
+            val map = mutableMapOf<Char, Int>()
+            group.forEach { personAnswers ->
+                personAnswers.forEach { answer ->
+                    map.merge(answer, 1){a,b -> a+b}
+                }
+            }
+            map.filter { it.value == group.count() }.size
+        }.sum().toString()
+    }
+}
