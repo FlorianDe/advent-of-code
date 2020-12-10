878c5b523bca7371a105ebf0e0c049bac4c7b06b
FlorianDe <florian.decker@alchemisten.ag>
2020-12-10 23:16:51 +0100

    Added solutions for day 01 of the advent coding calendar 2020.

---
+++ adventofcode/src/main/kotlin/de/florian/adventofcode/y2020/Day01.kt
@@ -0,0 +1,29 @@
+import de.florian.adventofcode.AoCDay
+
+fun main() { Day01().exec() }
+
+class Day01 : AoCDay() {
+    private fun convertInput(input: String) = input.split("\n").map { Integer.parseInt(it.trim()) }
+
+    private val numbers = convertInput(Inputs_2020.DAY_01)
+
+    override fun part1(): String {
+        numbers.forEach { a ->
+            numbers.forEach { b ->
+                if (a + b == 2020) return "${a * b}";
+            }
+        }
+        throw Error("Input broken.")
+    }
+
+    override fun part2(): String {
+        numbers.forEach { a ->
+            numbers.forEach { b ->
+                numbers.forEach { c ->
+                    if (a + b + c == 2020) return "${a * b * c}";
+                }
+            }
+        }
+        throw Error("Input broken.")
+    }
+}
