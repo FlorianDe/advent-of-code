dedd41482a7ec252f0ad2f5d71470de95ab88dda
FlorianDe <florian.decker@alchemisten.ag>
2020-12-10 23:18:39 +0100

    Added solutions for day 02 of the advent coding calendar 2020.

---
+++ adventofcode/src/main/kotlin/de/florian/adventofcode/y2020/Day02.kt
@@ -0,0 +1,41 @@
+import de.florian.adventofcode.AoCDay
+
+
+fun main() { Day02().exec() }
+
+class Day02 : AoCDay() {
+    companion object {
+        val SIMPLE_RANGE_PASSWORD_REGEX =  """(\d+)-(\d+) (\w): (.+)""".toRegex()
+    }
+    abstract class PasswordPolicy {
+        abstract fun valid(input: String): Boolean;
+    }
+    data class SimpleRangePolicy(val range: ClosedRange<Int>, val letter: Char) : PasswordPolicy() {
+        override fun valid(input: String): Boolean = range.contains(input.count { it == letter })
+    }
+    data class IndexedPasswordPolicy(val firstIndex: Int, val secondIndex: Int, val letter: Char) : PasswordPolicy() {
+        override fun valid(input: String): Boolean = (input[firstIndex-1] == letter) xor (input[secondIndex-1] == letter)
+    }
+    data class PolicyPasswordHolder(val policy: PasswordPolicy, val password: String)
+
+    private fun convertInput(input: String, policyCreator: (from: Int, to: Int, letter: Char) -> PasswordPolicy) = input.split("\n").map {
+        val (from, to, letter, password) = SIMPLE_RANGE_PASSWORD_REGEX.find(it.trim())!!.destructured
+        PolicyPasswordHolder(policyCreator(Integer.parseInt(from), Integer.parseInt(to), letter.first()), password)
+    }
+
+    override fun part1(): String {
+        return convertInput(Inputs_2020.DAY_02){ from, to, letter ->
+            SimpleRangePolicy(from..to, letter)
+        }.count {
+            it.policy.valid(it.password)
+        }.toString()
+    }
+
+    override fun part2(): String {
+        return convertInput(Inputs_2020.DAY_02){ from, to, letter ->
+            IndexedPasswordPolicy(from, to, letter)
+        }.count {
+            it.policy.valid(it.password)
+        }.toString()
+    }
+}
