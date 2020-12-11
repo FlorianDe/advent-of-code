37b7f865d4abcd00d4e607c51899a203827a052a
FlorianDe <florian.decker@alchemisten.ag>
2020-12-11 19:14:40 +0100

    [AOC2020] Refactored Day 05

--- adventofcode/src/main/kotlin/de/florian/adventofcode/y2020/Day05.kt
+++ adventofcode/src/main/kotlin/de/florian/adventofcode/y2020/Day05.kt
@@ -1,49 +1,34 @@
 import de.florian.adventofcode.AoCDay
+import kotlin.system.measureTimeMillis
 
 fun main() {
-    Day05().exec()
+    println("Took ${measureTimeMillis { Day05().exec()}} ms")
 }
 
 class Day05 : AoCDay() {
-    companion object {
-        const val ROW_BINARY_DEPTH = 7;
-        const val COL_BINARY_DEPTH = 3;
-        val seatDecoderMap: Map<Char, String> = mapOf(
-                'F' to "0",
-                'B' to "1",
-                'L' to "0",
-                'R' to "1"
-        )
-    }
+    private val replaceBinaryHigh: (str: String) -> String = {it.replace(Regex("[BR]"), "1")}
+    private val replaceBinaryLow: (str: String) -> String = {it.replace(Regex("[FL]"), "0")}
+    private val parseNumberFromBinary: (str: String ) -> Int = { it.toInt(2) }
 
-    data class BinarySeatHolder(val seatRow: Int, val seatCol: Int) {
-        val seatId: Int = seatRow*(ROW_BINARY_DEPTH+1)+seatCol
-
-        companion object {
-            fun decodeFromString(input: String) : BinarySeatHolder {
-                assert(input.length == ROW_BINARY_DEPTH + COL_BINARY_DEPTH)
-                val parseStringToBinary: (range: IntRange) -> Int = { range -> input.subSequence(range).map { seatDecoderMap[it]!! }.joinToString("").toInt(2) }
-                val row: Int = parseStringToBinary(0 until ROW_BINARY_DEPTH)
-                val col: Int = parseStringToBinary(ROW_BINARY_DEPTH until ROW_BINARY_DEPTH + COL_BINARY_DEPTH)
-                return BinarySeatHolder(row, col)
-            }
-        }
-    }
-
-    private fun convertInput(input: String) = input.split("\n").map { BinarySeatHolder.decodeFromString(it.trim()) }
-
-    private val decodedSeats = convertInput(Inputs_2020.DAY_05).sortedBy { it.seatId }
+    private val sortedDecodedSeats = Inputs_2020.DAY_05
+            .split("\n")
+            .map(replaceBinaryHigh)
+            .map(replaceBinaryLow)
+            .map(parseNumberFromBinary)
+            .sorted()
 
     override fun part1(): String {
-        return decodedSeats.last().seatId.toString()
+        return sortedDecodedSeats.last().toString()
     }
 
     override fun part2(): String {
-        val sortedSeatIds = decodedSeats.map { it.seatId }.toIntArray()
-        val first = sortedSeatIds.first()
-        for(i in 0 until sortedSeatIds.last()-first)
-            if(sortedSeatIds[i] != i+first)
-                return "${i+first}"
-        throw Error("Couldn't find any seat.")
+        val filterFirstAndLastElement:(index: Int, number: Int) -> Boolean = {idx: Int, _: Int -> (idx!=0 && idx!=sortedDecodedSeats.size-1) }
+
+        return sortedDecodedSeats
+                .filterIndexed(filterFirstAndLastElement)
+                .zipWithNext()
+                .findLast { p -> p.first+1 != p.second }
+                ?.first
+                ?.plus(1).toString()
     }
 }
