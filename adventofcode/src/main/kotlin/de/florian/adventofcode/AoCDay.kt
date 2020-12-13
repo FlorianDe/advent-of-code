4a6ef9121e79ca94ffe2db4f7079f091f2b89b5c
FlorianDe <florian.decker@alchemisten.ag>
2020-12-13 20:27:40 +0100

    [AOC2020] Changed fast impl. of Day 05

--- adventofcode/src/main/kotlin/de/florian/adventofcode/AoCDay.kt
+++ adventofcode/src/main/kotlin/de/florian/adventofcode/AoCDay.kt
@@ -1,11 +1,20 @@
 package de.florian.adventofcode
 
+import kotlin.system.measureNanoTime
+
 abstract class AoCDay {
     abstract fun part1(): String
     abstract fun part2(): String
 
     fun exec(){
-        println("Part1: ${this.part1()}")
-        println("Part2: ${this.part2()}")
+        val t1 = measureNanoTime {
+            println("Part1: ${this.part1()}")
+        }
+        println("Took: ${t1/1000000.0} ms.\n")
+
+        val t2 = measureNanoTime {
+            println("Part2: ${this.part2()}")
+        }
+        println("Took: ${t2/1000000.0} ms.")
     }
 }
--- adventofcode/src/main/kotlin/de/florian/adventofcode/y2020/fast_impl/Day05_fast.kt
+++ adventofcode/src/main/kotlin/de/florian/adventofcode/y2020/fast_impl/Day05_fast.kt
@@ -2,29 +2,44 @@ import de.florian.adventofcode.AoCDay
 import kotlin.system.measureTimeMillis
 
 fun main() {
-    println("Took ${measureTimeMillis { Day05_Fast().exec()}} ms")
+    println("\nAll took ${measureTimeMillis { Day05_Fast().exec()}} ms")
 }
 
 class Day05_Fast : AoCDay() {
     companion object{
-        const val MAX_ROWS: Int  = 7
-        const val MAX_COLS: Int  = 3
-        const val BIT_LENGTH: Int = MAX_ROWS + MAX_COLS
+        private const val MAX_ROWS: Int  = 7
+        private const val MAX_COLS: Int  = 3
+        private const val BIT_LENGTH: Int = MAX_ROWS + MAX_COLS
+        private const val WHITESPACE_LENGTH = 1
+        private const val BIT_LINE_SKIP = BIT_LENGTH+WHITESPACE_LENGTH
     }
 
     data class SeatDataHolder(val min: Int, val max:Int, val seats: Array<Boolean>)
 
+    private val seatInformation = calcSeatInformation()
+
     private fun calcSeatInformation(): SeatDataHolder{
-        val seatSize = 2 shl BIT_LENGTH
+        val seatSize = 2 shl (BIT_LENGTH-1)
         val maxSeatId: Int = seatSize - 1
+        val seatInput = Inputs_2020.DAY_05.toCharArray()
+        val seatRowsLength = seatInput.size / BIT_LINE_SKIP
+
         var min = maxSeatId
         var max = 0
         val seats: Array<Boolean> = Array(seatSize) { false }
 
-        for (line in Inputs_2020.DAY_05.split("\n")){
+        for (y in 0 until seatRowsLength-1){
             var seatId = 0
-            for (idx in line.indices){
-                seatId += (if(line[idx] == 'B' || line[idx] == 'R') 1 else 0) shl (BIT_LENGTH-idx-1)
+            for(x in 0 until BIT_LENGTH){
+                val idx = (y*BIT_LENGTH+y+x)
+                if(seatInput[idx] == 'B' || seatInput[idx] == 'R'){
+                    seatId = seatId or (1 shl (BIT_LENGTH-x-1))
+                }
+                //B -> 66 01000010
+                //R -> 82 01010010
+                //F -> 70 01000110
+                //L -> 76 01001100
+                //seatId = seatId or ((((-(seatInput[idx].toInt() and 4)) shr 2)+1)*(1 shl (BIT_LENGTH-x-1)))
             }
             if(seatId>max){
                 max = seatId
@@ -34,22 +49,18 @@ class Day05_Fast : AoCDay() {
             }
             seats[seatId] = true
         }
-
         return SeatDataHolder(min, max, seats)
     }
 
-    private val seatInformation = calcSeatInformation()
-
     override fun part1(): String {
-        return seatInformation.max.toString()
+        return calcSeatInformation().max.toString()
     }
 
     override fun part2(): String {
-        for(seatId in seatInformation.min until seatInformation.max - 1) {
-            if(seatInformation.seats[seatId] != seatInformation.seats[seatId+1]){
+        for(seatId in seatInformation.min until seatInformation.max - 1)
+            if(seatInformation.seats[seatId] != seatInformation.seats[seatId+1])
                 return (seatId+1).toString()
-            }
-        }
-        throw Error("No empty seat!")
+
+        return "No empty seat!"
     }
 }
