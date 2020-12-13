214002f180201ddfab19708e851a57b23cbb0b53
FlorianDe <florian.decker@alchemisten.ag>
2020-12-13 23:02:52 +0100

    [AOC2020] Added solutions for Day 09.

---
+++ adventofcode/src/main/kotlin/de/florian/adventofcode/y2020/Day09.kt
@@ -0,0 +1,52 @@
+import de.florian.adventofcode.AoCDay
+import java.math.BigInteger
+
+fun main() {
+    Day09().exec()
+}
+
+class Day09 : AoCDay() {
+
+    companion object {
+        const val PREAMBLE_SIZE = 25
+    }
+
+    private val numbers: Array<Long> = Inputs_2020.DAY_09.split("\n").map { it.trim().toLong() }.toTypedArray()
+
+    private fun calcInvalidNumber(): Long? {
+        for (i in PREAMBLE_SIZE until numbers.size) {
+            var valid = false
+            for (c1 in 1..PREAMBLE_SIZE)
+                for (c2 in 1..PREAMBLE_SIZE)
+                    if (c1 != c2)
+                        if (numbers[i - c1] + numbers[i - c2] == numbers[i])
+                            valid = true
+            if (!valid) return numbers[i]
+        }
+        return null
+    }
+
+    override fun part1(): String {
+        return calcInvalidNumber()?.toString() ?: throw Error("All inputs are valid!.")
+    }
+
+    override fun part2(): String {
+        val invalidNumber: BigInteger = calcInvalidNumber()?.toBigInteger() ?: throw Error("All inputs are valid!.")
+
+        for (startIdx in numbers.indices) {
+            val start = numbers[startIdx]
+            var min = start
+            var max = start
+            var sum: BigInteger = start.toBigInteger()
+            for (endIdx in startIdx + 1 until numbers.size) {
+                val end = numbers[endIdx]
+                sum = sum.plus(end.toBigInteger())
+                if (sum > invalidNumber) break
+                if (min > end) min = end
+                if (max < end) max = end
+                if (sum == invalidNumber) return (min + max).toString()
+            }
+        }
+        throw Error("No contiguous set of at least two numbers found.")
+    }
+}
