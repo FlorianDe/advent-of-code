1e74338ccb79a9d17ee002c02fb269e544d88fe5
FlorianDe <florian.decker@alchemisten.ag>
2020-12-15 21:32:46 +0100

    [AOC2020] Improved part 2 solution of Day 05 from O(n) to O(1).

--- adventofcode/src/main/kotlin/de/florian/adventofcode/y2020/fast_impl/Day05_fast.kt
+++ adventofcode/src/main/kotlin/de/florian/adventofcode/y2020/fast_impl/Day05_fast.kt
@@ -9,58 +9,42 @@ class Day05_Fast : AoCDay() {
     companion object{
         private const val MAX_ROWS: Int  = 7
         private const val MAX_COLS: Int  = 3
-        private const val BIT_LENGTH: Int = MAX_ROWS + MAX_COLS
         private const val WHITESPACE_LENGTH = 1
+        private const val BIT_LENGTH: Int = MAX_ROWS + MAX_COLS
         private const val BIT_LINE_SKIP = BIT_LENGTH+WHITESPACE_LENGTH
     }
 
-    data class SeatDataHolder(val min: Int, val max:Int, val seats: Array<Boolean>)
+    data class SeatDataHolder(val min: Long, val max:Long, val missingSeat: Long)
 
     private val seatInformation = calcSeatInformation()
 
     private fun calcSeatInformation(): SeatDataHolder{
-        val seatSize = 2 shl (BIT_LENGTH-1)
-        val maxSeatId: Int = seatSize - 1
+        val maxSeatId: Long = ((2L shl (BIT_LENGTH-1)) - 1).also {
+            if(it > 4294967295L) throw Error("This algorithm is only stable for airplanes with a maximum seat id of 1/2*(sqrt(8*(Long.MAX_VALUE)+1)-1) which is 4294967295.")
+        }
         val seatInput = Inputs_2020.DAY_05.toCharArray()
         val seatRowsLength = seatInput.size / BIT_LINE_SKIP
+        var seatSum: Long = 0
+        var min: Long  = maxSeatId
+        var max: Long  = 0
 
-        var min = maxSeatId
-        var max = 0
-        val seats: Array<Boolean> = Array(seatSize) { false }
-
-        for (y in 0 until seatRowsLength-1){
-            var seatId = 0
-            for(x in 0 until BIT_LENGTH){
-                val idx = (y*BIT_LENGTH+y+x)
-                if(seatInput[idx] == 'B' || seatInput[idx] == 'R'){
-                    seatId = seatId or (1 shl (BIT_LENGTH-x-1))
-                }
-                //B -> 66 01000010
-                //R -> 82 01010010
-                //F -> 70 01000110
-                //L -> 76 01001100
-                //seatId = seatId or ((((-(seatInput[idx].toInt() and 4)) shr 2)+1)*(1 shl (BIT_LENGTH-x-1)))
+        for (y in 0..seatRowsLength){
+            var seatId = 0L
+            for(x in 0 until BIT_LENGTH) {
+                seatId = (seatId shl 1) + (seatInput[(y*BIT_LENGTH+y+x)].toInt() % 7) % 2
             }
-            if(seatId>max){
-                max = seatId
-            }
-            if(seatId<min){
-                min = seatId
-            }
-            seats[seatId] = true
+            if(seatId>max) max = seatId
+            if(seatId<min) min = seatId
+            seatSum += seatId
         }
-        return SeatDataHolder(min, max, seats)
+        return SeatDataHolder(min, max, (((min+max)*(max-min+1)) shr 1)-seatSum)
     }
 
     override fun part1(): String {
-        return calcSeatInformation().max.toString()
+        return seatInformation.max.toString()
     }
 
     override fun part2(): String {
-        for(seatId in seatInformation.min until seatInformation.max - 1)
-            if(seatInformation.seats[seatId] != seatInformation.seats[seatId+1])
-                return (seatId+1).toString()
-
-        return "No empty seat!"
+        return seatInformation.missingSeat.toString();
     }
 }
