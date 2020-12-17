336e8d563a449c811f48b64644a46e2fdda30c29
FlorianDe <florian.decker@alchemisten.ag>
2020-12-17 19:47:51 +0100

    [AOC2020] Added solutions for Day 13.

---
+++ adventofcode/src/main/kotlin/de/florian/adventofcode/y2020/Day13.kt
@@ -0,0 +1,44 @@
+import de.florian.adventofcode.AoCDay
+import kotlin.math.ceil
+
+fun main() {
+    Day13().exec()
+}
+
+class Day13 : AoCDay() {
+    data class Bus(val id: Long, val offset: Long)
+    data class BusInformation(val earliestTs: Long, val busses: List<Bus>)
+
+    private val itinerary: BusInformation = Inputs_2020.DAY_13.split("\n").let { line ->
+        BusInformation(
+                line.first().toLong(),
+                line.last().split(",").map { it.trim() }.mapIndexedNotNull { idx, value ->  if(value == "x")  null else Bus(value.toLong(), idx.toLong()) }
+        )
+    }
+
+    override fun part1(): String {
+        return itinerary.busses
+                .map { bus -> bus to ceil(itinerary.earliestTs/bus.id.toDouble())*bus.id - itinerary.earliestTs }
+                .minBy { it.second }
+                ?.let { it.first.id * it.second }
+                ?.toInt()
+                .toString()
+    }
+
+    /**
+     * Chinese remainder theorem
+     * Solved by Sieving
+     */
+    override fun part2(): String {
+        var ts = 0L
+        var step = itinerary.busses.first().id
+        for (i in 1 until itinerary.busses.size) {
+            val bus = itinerary.busses[i]
+            while ( (ts+bus.offset) % bus.id != 0L){
+                ts += step
+            }
+            step *= bus.id
+        }
+        return ts.toString()
+    }
+}
--- adventofcode/src/main/kotlin/de/florian/adventofcode/y2020/Inputs.kt
+++ adventofcode/src/main/kotlin/de/florian/adventofcode/y2020/Inputs.kt
@@ -2,6 +2,9 @@
 abstract class Inputs_2020 {
     companion object {
 
+        const val DAY_13 = """1006401
+17,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,449,x,x,x,x,x,x,x,23,x,x,x,x,13,x,x,x,x,x,19,x,x,x,x,x,x,x,x,x,x,x,607,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,29"""
+
         const val DAY_12 = """W5
 F66
 S4
