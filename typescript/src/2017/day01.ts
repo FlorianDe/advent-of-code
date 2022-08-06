c55ea6ae7d5b246f0efefcea246aad28749e4ad8
FlorianDe <florian.decker@alchemisten.ag>
2022-08-06 03:30:50 +0200

    [AOC2017][Typescript] Added solutions for Day 1 and project setup for aoc typescript.

---
+++ typescript/src/2017/day01.ts
@@ -0,0 +1,24 @@
+import {AocDay, Solutions} from "../aoc";
+
+export class Day01 extends AocDay {
+	constructor() {
+		super(2017, 1)
+	}
+	private calcCaptcha(nextDigitIndexSupplier: (idx: number, inputLength: number) => number): number {
+		const inputLength = this.input.split("").length
+		let sum = 0;
+		for (let i = 0; i < inputLength; i++) {
+			if (this.input[i] === this.input[nextDigitIndexSupplier(i, inputLength)]) {
+				sum += Number.parseInt(this.input[i]);
+			}
+		}
+		return sum;
+	}
+
+	solve(): Solutions {
+		return {
+			part1: this.calcCaptcha((idx, inputLength) => (idx + 1) % inputLength),
+			part2: this.calcCaptcha((idx, inputLength) => (idx + (inputLength/2)) % inputLength),
+		};
+	}
+}
