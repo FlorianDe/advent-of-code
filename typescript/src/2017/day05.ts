d84109e86fee29685cda4e659bb661dc8f526650
FlorianDe <florian.decker@alchemisten.ag>
2022-08-09 02:28:23 +0200

    [AOC2017][Typescript] Added solutions for Day 5.

---
+++ typescript/src/2017/day05.ts
@@ -0,0 +1,32 @@
+import {AocDay, Solutions} from "../aoc";
+
+export class Day05 extends AocDay {
+	constructor() {
+		super(2017, 5);
+	}
+
+	solve(): Solutions {
+		const parsedInstructions = this.input.split("\n").map(n => Number.parseInt(n, 10));
+
+		const executeJumps = (isPart2?: boolean): number => {
+			const instructions = [...parsedInstructions];
+			let idx = 0, steps = 0;
+			for (; 0 <= idx && idx < instructions.length; steps++) {
+				idx += isPart2 && instructions[idx] >= 3 ? instructions[idx]-- : instructions[idx]++;
+			}
+			return steps;
+		};
+
+		return {
+			part1: executeJumps(),
+			part2: executeJumps(true),
+		};
+	}
+}
+
+
+
+
+
+
+
