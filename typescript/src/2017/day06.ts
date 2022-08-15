8cf164783891f321510c910ceb7052f096064329
FlorianDe <florian.decker@alchemisten.ag>
2022-08-15 12:13:57 +0200

    [AOC2017][Typescript] Added solutions for Day 6.

---
+++ typescript/src/2017/day06.ts
@@ -0,0 +1,38 @@
+import {AocDay, Solutions} from "../aoc";
+
+export class Day06 extends AocDay {
+	constructor() {
+		super(2017, 6);
+	}
+
+	solve(): Solutions {
+		const memoryBanks = this.input.split(/\s+/).map(cell => Number.parseInt(cell, 10));
+
+		const seenMemoryConfigs = new Map<string, number>();
+		let cycles = 0;
+		let curConfig = [...memoryBanks];
+		while (!seenMemoryConfigs.has(curConfig.join())) {
+			seenMemoryConfigs.set(curConfig.join(), cycles);
+			const iterations = Math.max(...curConfig);
+			const idxOfMax = curConfig.indexOf(iterations);
+			const nextConfig = [...curConfig];
+			nextConfig[idxOfMax] = 0;
+			for (let i = 1; i <= iterations; i++) {
+				nextConfig[(idxOfMax + i) % nextConfig.length]++;
+			}
+			curConfig = nextConfig;
+			cycles++;
+		}
+		return {
+			part1: cycles,
+			part2: cycles - seenMemoryConfigs.get(curConfig.join())!!,
+		};
+	}
+}
+
+
+
+
+
+
+
