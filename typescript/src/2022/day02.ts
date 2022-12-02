1624c5d35fd05c25724385856a39eb91261dfe12
FlorianDe <florian.decker@alchemisten.ag>
2022-12-02 17:24:07 +0100

    [AOC2022][Typescript] Added solutions for Day 2 + short version with mathematical solution.

---
+++ typescript/src/2022/day02.ts
@@ -0,0 +1,94 @@
+import {AocDay, Solutions} from "../aoc";
+
+type GuideSymbolsLeft = 'A' | 'B' | 'C'
+type GuideSymbolsRight = 'X' | 'Y' | 'Z'
+const HandShapes = [ 'Rock',  'Paper', 'Scissors'] as const;
+type HandShape = typeof HandShapes[number];
+const ShapeToPoints: Record<HandShape, number> = {
+	Rock: 1,
+	Paper: 2,
+	Scissors: 3
+};
+const PointsToShape: Record<number, HandShape> = Object.fromEntries(HandShapes.map((key) => [ShapeToPoints[key], key]));
+type Round = {
+	op: HandShape;
+	you: HandShape;
+}
+type RoundOutcome = 'Lost' | 'Draw' | 'Won'
+const RoundOutcomePoints: Record<RoundOutcome, number> = {
+	Lost: 0,
+	Draw: 3,
+	Won: 6
+};
+export class Day02 extends AocDay {
+	constructor() {
+		super(2022, 2);
+	}
+	calcRoundOutcome(round: Round): RoundOutcome {
+		if(round.op === round.you){
+			return 'Draw';
+		}
+		if (ShapeToPoints[round.op] % 3 === ShapeToPoints[round.you] - 1){
+			return 'Won';
+		}
+		return 'Lost';
+	}
+
+	calculateScore(rounds: Round[]): number {
+		return rounds.reduce<number>((acc, curRound) => acc+ShapeToPoints[curRound.you]+RoundOutcomePoints[this.calcRoundOutcome(curRound)],0);
+	}
+
+	getMisreadGuideRounds(guideLines: string[]): Round[] {
+		const GuideHandShapeMap : Record<GuideSymbolsLeft | GuideSymbolsRight, HandShape> = {
+			A: 'Rock',
+			B: 'Paper',
+			C: 'Scissors',
+			X: 'Rock',
+			Y: 'Paper',
+			Z: 'Scissors'
+		};
+		return guideLines.map<Round>(line => {
+			const hands = line.split(" ");
+			return {
+				op: GuideHandShapeMap[hands[0] as GuideSymbolsLeft],
+				you: GuideHandShapeMap[hands[1] as GuideSymbolsRight],
+			};
+		});
+	}
+	getRealGuideRounds(guideLines: string[]): Round[] {
+		const GuideHandShapeMap : Record<GuideSymbolsLeft, HandShape> = {
+			A: 'Rock',
+			B: 'Paper',
+			C: 'Scissors'
+		};
+		const GuideOutcomeMap : Record<GuideSymbolsRight, RoundOutcome> = {
+			X: 'Lost',
+			Y: 'Draw',
+			Z: 'Won'
+		};
+		const calculateHandShapeForOutcome = (op: HandShape, roundOutcome: RoundOutcome): HandShape => {
+			let handMove = 0; // Draw
+			if(roundOutcome === 'Won'){
+				handMove = 1;
+			} else if(roundOutcome === 'Lost') {
+				handMove = -1;
+			}
+			return PointsToShape[(ShapeToPoints[op]-1 + handMove + 3) % 3 + 1];
+		};
+		return guideLines.map<Round>(line => {
+			const hands = line.split(" ");
+			const opHand = GuideHandShapeMap[hands[0] as GuideSymbolsLeft];
+			return {
+				op: opHand,
+				you: calculateHandShapeForOutcome(opHand, GuideOutcomeMap[hands[1] as GuideSymbolsRight])
+			};
+		});
+	}
+	solve(): Solutions {
+		const guideLines = this.input.split("\n");
+		return {
+			part1: this.calculateScore(this.getMisreadGuideRounds(guideLines)),
+			part2: this.calculateScore(this.getRealGuideRounds(guideLines)),
+		};
+	}
+}
---
+++ typescript/src/2022/day02_short.ts
@@ -0,0 +1,33 @@
+import {AocDay, Solutions} from "../aoc";
+
+export class Day02 extends AocDay {
+	constructor() {
+		super(2022, 2);
+	}
+
+	solve(): Solutions {
+		type SymbolMapFn = ([ops, you]: number[]) => number[];
+		const SYMBOL_MAP = Object.fromEntries(['A', 'B', 'C', 'X', 'Y', 'Z'].map((e, idx) => [e, idx % 3]));
+		const symbolsToNumber = ([ops, you]: string[]): number[] => [SYMBOL_MAP[ops], SYMBOL_MAP[you]];
+		const remEuclid = (n: number, mod: number) => ((n % mod) + mod) % mod;
+		const sum = (acc: number, cur: number): number => acc+cur;
+		const roundScore = ([ops, you]: number[]): number => (2 - remEuclid(ops - you + 1, 3)) * 3 + you+1;
+		const identity: SymbolMapFn = (symbols) => symbols;
+		const outcomeToSymbol: SymbolMapFn = ([ops, you]) => [ops, remEuclid(ops + (you-1), 3)];
+		const totalScore = (rounds: number[][], guideTranslation: SymbolMapFn): number => rounds
+			.map(guideTranslation)
+			.map(roundScore)
+			.reduce(sum);
+
+		const rounds = this.input.split('\n')
+			.map(l => l.trim().split(' '))
+			.map(symbolsToNumber);
+
+		return {
+			part1: totalScore(rounds, identity),
+			part2: totalScore(rounds, outcomeToSymbol),
+		};
+	}
+}
+
+console.log(new Day02().solve());
