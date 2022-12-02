632f0ae28fcfd94b20321b1e5769db21c64763da
FlorianDe <florian.decker@alchemisten.ag>
2022-12-02 17:22:43 +0100

    [AOC2022][Typescript] Added solutions for Day 1 and updated the README.

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,10 +1,12 @@
 import { performance } from 'perf_hooks';
-import * as y2017 from './2017';
+// import * as y2017 from './2017';
+import * as y2022 from './2022';
 
-Object.values(y2017).forEach(AocDay => {
+
+
+Object.values(y2022).forEach(AocDay => {
 	const start = performance.now();
 	console.log(`${new AocDay()}`);
 	const end = performance.now();
 	console.log(`Calculation time: ${(end-start).toFixed(3)}ms`);
 });
-
