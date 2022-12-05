f8b6bae3786e74eb648cad7ffaf54ada5cd39805
FlorianDe <florian.decker@alchemisten.ag>
2022-12-05 21:29:41 +0100

    [AOC2022][Typescript] Fixed Year type to not allow any years below 2015.

--- typescript/src/aoc/types.ts
+++ typescript/src/aoc/types.ts
@@ -1,7 +1,6 @@
-type ZeroToFive = 0|1|2|3|4|5
 type Digits = 1|2|3|4|5|6|7|8|9
-export type Year = `201${ZeroToFive}` | `20${Digits}${0|Digits}`
-export type Day =  `${Digits}`| `0${Digits}` | `1${0|Digits}` | `2${ZeroToFive}`
+export type Year = `201${5|6|7|8|9}` | `20${2|3|4|5|6|7|8|9}${0|Digits}`
+export type Day =  `${Digits}`| `0${Digits}` | `1${0|Digits}` | `2${0|1|2|3|4|5}`
 
 export function assertValidDay(day: number | string | Day): asserts day is Day {
 	const numDay: number = (typeof day !== 'number') ? parseInt(day, 10) : day;
@@ -16,4 +15,3 @@ export function assertValidYear(year: number | string | Year): asserts year is Y
 		throw Error("The provided year is not a valid integer between 2015 and 2099");
 	}
 }
-
