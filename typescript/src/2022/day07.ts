92707dd05612811632fcd07535df4f3a4e8ec560
FlorianDe <florian.decker@alchemisten.ag>
2022-12-10 01:37:09 +0100

    [AOC2022][Typescript] Removed typo in Day 8.

--- typescript/src/2022/day07.ts
+++ typescript/src/2022/day07.ts
@@ -4,30 +4,6 @@ import {AocDay, AocDayDecorator, SolutionParts, sum} from "../aoc";
 export class Day07 extends AocDay {
 
 	solveImpl(): SolutionParts {
-		const input = `$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k`;
-
 		const ROOT_NODE = "/";
 		type NodePath = string;
 		type Node = { parent: NodePath | undefined, children: NodePath[] | undefined, size: number, isFile: boolean }
--- typescript/src/aoc/math/point.ts
+++ typescript/src/aoc/math/point.ts
@@ -1,63 +1,84 @@
 import {IEquality, IHashable} from "../collections/hash";
 
-export class Point2D implements IHashable, IEquality<Point2D>{
-	private readonly x: number;
-	private readonly y: number;
+export class Point2D implements IHashable, IEquality<Point2D> {
+	get x(): number {
+		return this._x;
+	}
+
+	get y(): number {
+		return this._y;
+	}
+
+	private readonly _x: number;
+	private readonly _y: number;
 
 	constructor(x: number, y: number) {
-		this.x = x;
-		this.y = y;
+		this._x = x;
+		this._y = y;
 	}
 
 	public static fromArray(elements: number[]) {
-		if(!elements || elements.length !== 2){
+		if (!elements || elements.length !== 2) {
 			throw Error(`To create a ${this.constructor.name} from an array exactly 2 elements have to be specified.`);
 		}
-		const [x,y] = elements;
-		return new Point2D(x,y);
+		const [x, y] = elements;
+		return new Point2D(x, y);
 	}
+
 	public static zero(): Point2D {
-		return new Point2D(0,0);
+		return new Point2D(0, 0);
 	}
 
 	public toArray(): number[] {
-		return [this.x, this.y];
+		return [this._x, this._y];
 	}
 
 	public add(other: Point2D): Point2D {
-		return new Point2D(this.x+other.x, this.y+other.y);
+		return new Point2D(this._x + other._x, this._y + other._y);
 	}
 
 	public sub(other: Point2D): Point2D {
-		return new Point2D(this.x-other.x, this.y-other.y);
-	}
-	public mul(other: Point2D): number {
-		return this.x*other.x+this.y*other.y;
-	}
-	public mulScalar(scalar: number): Point2D{
-		return new Point2D(scalar*this.x, scalar*this.y);
+		return new Point2D(this._x - other._x, this._y - other._y);
 	}
 
-	public rotate(angle: number, other?: Point2D, angleInRadians?: boolean): Point2D{
+	public mul(other: Point2D): number {
+		return this._x * other._x + this._y * other._y;
+	}
+
+	public mulScalar(scalar: number): Point2D {
+		return new Point2D(scalar * this._x, scalar * this._y);
+	}
+
+	public abs(): Point2D {
+		return new Point2D(Math.abs(this._x), Math.abs(this._y));
+	}
+
+	public sign(): Point2D {
+		return new Point2D(Math.sign(this._x), Math.sign(this._y));
+	}
+
+	public copy(): Point2D {
+		return new Point2D(this._x, this._y);
+	}
+
+	public rotate(angle: number, other?: Point2D, angleInRadians?: boolean): Point2D {
 		const angleRadians = angleInRadians ? angle : angle * (Math.PI / 180);
 		const s = Math.sin(angleRadians);
 		const c = Math.cos(angleRadians);
 		const rotationPoint = other ?? Point2D.zero();
 
 		const pTranslated = this.sub(rotationPoint);
-		const rotated = new Point2D(pTranslated.x*c - pTranslated.y*s, pTranslated.x*s+pTranslated.y*c);
+		const rotated = new Point2D(pTranslated._x * c - pTranslated._y * s, pTranslated._x * s + pTranslated._y * c);
 		return rotated.add(rotationPoint);
 	}
 
 	equals(other: Point2D): boolean {
-		return this.x === other.x && this.y === other.y;
+		return this._x === other._x && this._y === other._y;
 	}
 
-	hashCode(): number | string {
-		return `x${this.x}y${this.y}`;
+	hashCode(): string {
+		return `x${this._x}y${this._y}`;
 	}
-
-
 }
 
 export const DiagonalGridDirections = [
