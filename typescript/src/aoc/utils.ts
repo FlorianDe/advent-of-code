795a5bd8c925384e98da6b1362ad7d7ba665f339
FlorianDe <florian.decker@alchemisten.ag>
2023-12-06 20:07:20 +0100

    [AOC2023][Typescript] Added solutions for Day 6.

--- typescript/src/aoc/utils.ts
+++ typescript/src/aoc/utils.ts
@@ -19,6 +19,7 @@ export namespace Filters {
 }
 
 export namespace Arrays {
+	export const transpose = <T>(arr: T[][]): T[][] => arr[0].map((_, colIndex) => arr.map(row => row[colIndex]));
 	export const chunk = <T>(arr: T[], size: number): T[][] => Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
 		arr.slice(i * size, i * size + size)
 	);
