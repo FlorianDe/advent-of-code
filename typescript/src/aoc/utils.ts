f0e7f88451180a42936884fe55437da93ddc3695
FlorianDe <florian.decker@alchemisten.ag>
2024-01-11 14:53:55 +0700

    [AOC2023][Typescript] Added solutions for Day 9.

--- typescript/src/aoc/utils.ts
+++ typescript/src/aoc/utils.ts
@@ -2,6 +2,13 @@ import {readFileSync} from 'fs';
 import * as path from 'path';
 import {assertValidDay, assertValidYear, Day, Year} from "./types";
 
+export namespace Iterators {
+	export function* map<T1, T2>(iterable: IterableIterator<T1>, callback: (value: T1) => T2) {
+		for (const x of iterable) {
+			yield callback(x);
+		}
+	}
+}
 export namespace Sets {
 	export const intersection = <T>(a: Set<T> | Iterable<T>, b: Set<T>): Set<T> => new Set([...a].filter(x => b.has(x)));
 }
@@ -23,6 +30,26 @@ export namespace Arrays {
 	export const chunk = <T>(arr: T[], size: number): T[][] => Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
 		arr.slice(i * size, i * size + size)
 	);
+	export const windowed = <T>(values: T[], windowSize: number, start = 0, end = Infinity, step = 1): IterableIterator<T[]> => {
+		let nextIndex = start;
+		let iterationCount = 0;
+
+		return {
+			[Symbol.iterator](): IterableIterator<T[]> {
+				return this;
+			},
+			next() {
+				const endIndex = nextIndex + windowSize;
+				if (values && values.length > 0 && endIndex <= end && endIndex <= values.length) {
+					iterationCount++;
+					const value = values.slice(nextIndex, Math.min(endIndex, values.length));
+					nextIndex += step;
+					return { value, done: false };
+				}
+				return { value: iterationCount, done: true };
+			}
+		};;
+	};
 }
 
 export namespace Files {
