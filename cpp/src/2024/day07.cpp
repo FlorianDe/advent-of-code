74ce84f69e465598f25064ad8f029fa703ea56db
FlorianDe <FlorianDe@users.noreply.github.com>
2024-12-07 23:20:12 +0100

    [AOC2024][CPP] Refactored solution for Day 7.
    Added Opertion enum.

--- cpp/src/2024/day07.cpp
+++ cpp/src/2024/day07.cpp
@@ -1,84 +1,86 @@
 #include "../utils/containers.cpp"
 #include "../utils/files.cpp"
+#include <algorithm>
 #include <array>
 #include <chrono>
 #include <functional>
+#include <numeric>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
 
 struct Equation {
-	int64_t result;
-	std::vector<int64_t> operands;
+    int64_t result;
+    std::vector<int64_t> operands;
 
-	static Equation toEquation(const std::string& line) {
-		std::istringstream iss(line);
-		Equation eq;
-		std::string leftPart;
-		std::getline(iss, leftPart, ':');
-		eq.result = std::stoll(leftPart);
+    static Equation toEquation(const std::string& line)
+    {
+        std::istringstream iss(line);
+        std::string leftPart;
+        std::getline(iss, leftPart, ':');
 
-		eq.operands = std::vector<int64_t>(std::istream_iterator<int64_t>(iss), std::istream_iterator<int64_t>());
-
-		return eq;
-	}
+        return {
+            std::stoll(leftPart),
+            std::vector<int64_t>(std::istream_iterator<int64_t>(iss), std::istream_iterator<int64_t>())
+        };
+    }
 };
-using OperationMap = std::unordered_map<std::string, std::function<int64_t(int64_t, int64_t)>>;
+enum class Operation { 
+	Add,
+    Multiply,
+    Concatenate
+};
+using OperationMap = std::unordered_map<Operation, std::function<int64_t(int64_t, int64_t)>>;
 
-auto calculateCalibration(const std::vector<Equation>& equations, const OperationMap& operations) {
-	auto isValidEquation = [&operations](const Equation& equation){
-		std::unordered_set<int64_t> results{*equation.operands.begin()};
-		auto operands = equation.operands;
-		operands.erase(operands.begin());
-		for (auto nextOperand : operands) {
-			std::unordered_set<int64_t> newResults;
-			newResults.reserve(results.size() * operations.size());
-			for (auto prevOperand : results) {
-				for (const auto& [op, func] : operations) {
-					auto result = func(prevOperand, nextOperand);
-					if(result <= equation.result){
-						newResults.insert(result);
-					}
-				}
-			}
-			results.swap(newResults);
-		}
-		return results.contains(equation.result);
-	};
+auto calculateCalibration(const std::vector<Equation>& equations, const std::vector<Operation>&& useOps)
+{
+    const OperationMap operations {
+        { Operation::Add, [](int64_t a, int64_t b) { return a + b; } },
+        { Operation::Multiply, [](int64_t a, int64_t b) { return a * b; } },
+        { Operation::Concatenate, [](int64_t a, int64_t b) { return a * std::pow<int64_t, int64_t>(10, std::log10(b) + 1) + b; } }
+    };
 
-	auto validEquationsResultSum = 0LL;
-	for (const auto& equation : equations) {
-		if(isValidEquation(equation)){
-			validEquationsResultSum+=equation.result;
-		}
-	}
+    auto isValidEquation = [&operations, &useOps](const Equation& equation) {
+        std::unordered_set<int64_t> results { *equation.operands.begin() };
+        auto operands = equation.operands;
+        operands.erase(operands.begin());
+        for (auto nextOperand : operands) {
+            std::unordered_set<int64_t> newResults;
+            newResults.reserve(results.size() * operations.size());
+            for (auto prevOperand : results) {
+                for (const auto& op : useOps) {
+                    auto result = operations.at(op)(prevOperand, nextOperand);
+                    if (result <= equation.result) {
+                        newResults.insert(result);
+                    }
+                }
+            }
+            results.swap(newResults);
+        }
+        return results.contains(equation.result);
+    };
 
-	return validEquationsResultSum;
+    return std::accumulate(equations.begin(), equations.end(), 0LL,
+        [&isValidEquation](auto sum, const Equation& equation) {
+            return isValidEquation(equation) ? sum + equation.result : sum;
+        });
 }
 
 int part02() { return 0L; }
 
-int main() {
-	auto start = std::chrono::high_resolution_clock::now();
-	auto file = readFile("../generated/aoc/inputs/2024/day07.txt");
+int main()
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    auto file = readFile("../generated/aoc/inputs/2024/day07.txt");
 
-	auto equations = file.getLines() | std::views::transform(Equation::toEquation) | std::ranges::to<std::vector>();
+    auto equations = file.getLines() | std::views::transform(Equation::toEquation) | std::ranges::to<std::vector>();
 
-	OperationMap basicOperations{
-			{"+", [](int64_t a, int64_t b) { return a + b; }},
-			{"*", [](int64_t a, int64_t b) { return a * b; }},
-	};
-	OperationMap extendedOperations(basicOperations);
-	extendedOperations.insert(
-			{{"||", [](int64_t a, int64_t b) { return std::stoll(std::to_string(a) + std::to_string(b)); }}});
+    auto part1 = calculateCalibration(equations, { Operation::Add, Operation::Multiply });
+    auto part2 = calculateCalibration(equations, { Operation::Add, Operation::Multiply, Operation::Concatenate });
 
-	auto part1 = calculateCalibration(equations, basicOperations);
-
-	auto part2 = calculateCalibration(equations, extendedOperations);
-
-	auto end = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<double, std::milli> duration_ms = end - start;
-	std::cout << "Execution time: " << duration_ms.count() << " ms" << std::endl;
-	std::cout << "Part 01: " << (part1) << std::endl;
-	std::cout << "Part 02: " << (part2) << std::endl;
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> duration_ms = end - start;
+    std::cout << "Execution time: " << duration_ms.count() << " ms" << std::endl;
+    std::cout << "Part 01: " << (part1) << std::endl;
+    std::cout << "Part 02: " << (part2) << std::endl;
 }
