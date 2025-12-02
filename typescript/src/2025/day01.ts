d6a1b773a0d1537519168f8ea2bcfba5dc24f946
FlorianDe <FlorianDe@users.noreply.github.com>
2025-12-02 21:34:22 +0100

    [AOC2025][TS] Added prettierrc and formatted code

--- typescript/src/2025/day01.ts
+++ typescript/src/2025/day01.ts
@@ -1,24 +1,19 @@
-import { AocDay, AocDayDecorator, SolutionParts } from "../aoc";
-import { Maths } from "../aoc/math";
+import { AocDay, AocDayDecorator, SolutionParts } from '../aoc';
+import { Maths } from '../aoc/math';
 
-@AocDayDecorator("2025", "1")
+@AocDayDecorator('2025', '1')
 export class Day01 extends AocDay {
   solveImpl(): SolutionParts {
-    const isAtZero = (params: { pos: number; dialSize: number }) =>
-      Maths.Arithmetics.mod(params.pos, params.dialSize) === 0;
+    const isAtZero = (params: { pos: number; dialSize: number }) => Maths.Arithmetics.mod(params.pos, params.dialSize) === 0;
 
     const rotations = this.input
-      .replaceAll("R", "")
-      .replaceAll("L", "-")
-      .split("\n")
+      .replaceAll('R', '')
+      .replaceAll('L', '-')
+      .split('\n')
       .map((s) => Number.parseInt(s.trim(), 10));
 
     const processRotations = (
-      countFn: (params: {
-        pos: number;
-        dialSize: number;
-        rotation: number;
-      }) => number,
+      countFn: (params: { pos: number; dialSize: number; rotation: number }) => number,
       startPos: number = 50,
       dialSize: number = 100
     ) =>
@@ -32,19 +27,11 @@ export class Day01 extends AocDay {
       ).count;
 
     return {
-      part1: processRotations(({ pos, rotation, dialSize }) =>
-        isAtZero({ pos: pos + rotation, dialSize }) ? 1 : 0
-      ),
+      part1: processRotations(({ pos, rotation, dialSize }) => (isAtZero({ pos: pos + rotation, dialSize }) ? 1 : 0)),
       part2: processRotations(({ pos, rotation, dialSize }) => {
         if (rotation === 0) return 0;
-        const { from, to } =
-          rotation > 0
-            ? { from: pos + 1, to: pos + rotation }
-            : { from: pos + rotation, to: pos - 1 };
-        return Math.max(
-          0,
-          Math.floor(to / dialSize) - Math.ceil(from / dialSize) + 1
-        );
+        const { from, to } = rotation > 0 ? { from: pos + 1, to: pos + rotation } : { from: pos + rotation, to: pos - 1 };
+        return Math.max(0, Math.floor(to / dialSize) - Math.ceil(from / dialSize) + 1);
       }),
     };
   }
